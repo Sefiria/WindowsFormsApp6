5d0d4cf49606a761ebbca95cf2928acab5bc47b1
sauveur.dominguez <sauveur.dominguez@zf.com>
2024-04-30 11:45:39 +0200

    stable

--- Tooling_Mapper/Form1.cs
+++ Tooling_Mapper/Form1.cs
@@ -22,6 +22,7 @@ namespace Tooling_Mapper
         List<Segment> Segments = new List<Segment>();
         List<(string text, int time)> Notifs = new List<(string text, int time)>();
         int SelectedSegmentIndex = -1, SelectedPointIndex = -1;
+        (int, bool, bool) SelectedSegment;
         PointF SelectedOffset;
         PointF Center, Cam;
         float CamSpeed = 5F;
@@ -49,6 +50,9 @@ namespace Tooling_Mapper
         private static string ToValidJson(string input) => input.Replace("=", ":").Replace("X", "\"X\"").Replace("Y", "\"Y\"");
         private void Update(object sender, EventArgs e)
         {
+            if (!Render.Parent.Focused)
+                return;
+
             (bool _z, bool _q, bool _s, bool _d) = KB.ZQSD();
             if (_z) Cam.Y -= CamSpeed;
             if (_q) Cam.X -= CamSpeed;
@@ -57,7 +61,7 @@ namespace Tooling_Mapper
 
             if (KB.LeftCtrl)
             {
-                if(KB.IsKeyPressed(KB.Key.Z) && Segments.Count > 0)
+                if (KB.IsKeyPressed(KB.Key.Z) && Segments.Count > 0)
                     Segments.RemoveAt(Segments.Count - 1);
                 else if (KB.IsKeyPressed(KB.Key.C) && Segments.Count > 0)
                 {
@@ -67,7 +71,7 @@ namespace Tooling_Mapper
                         Clipboard.SetText(data);
                         Notifs.Add(("Copied !", 0));
                     }
-                    catch(Exception ex)
+                    catch (Exception ex)
                     {
                         MessageBox.Show($"Clipboard Copy failed : {ex}");
                     }
@@ -138,30 +142,33 @@ namespace Tooling_Mapper
             bool left_pressed = MouseStates.IsButtonPressed(MouseButtons.Left);
             bool right_pressed = MouseStates.IsButtonPressed(MouseButtons.Right);
             bool left_down = MouseStates.IsButtonDown(MouseButtons.Left);
+            var ms = MouseStates.Position.MinusF(Center).PlusF(Cam);
 
 
-            if (KB.LeftCtrl)
+            if (KB.LeftAlt)
             {
                 bool col(PointF pt)
                 {
-                    return Maths.CollisionPointCercle(Center.MinusF(MouseStates.Position), pt.X - point_size / 2F, pt.Y - point_size / 2F, point_size);
+                    return Maths.CollisionPointCercle(ms, pt.X, pt.Y, point_size);
                 }
                 var segs = Segments.Select(s => (Segments.IndexOf(s), col(s.A), col(s.B))).Where(i => i.Item2 || i.Item3);
                 if (segs.Count() > 0)
                 {
-                    var SelectedSegment = segs.MinBy(i => Maths.Distance(i.Item2 ? Segments[i.Item1].A : Segments[i.Item1].B, Center.MinusF(MouseStates.Position)));
+                    SelectedSegment = segs.MinBy(i => Maths.Distance(i.Item2 ? Segments[i.Item1].A : Segments[i.Item1].B, ms));
+                    SelectedPointIndex = SelectedSegment.Item2 ? 0 : 1;
                     if (left_pressed)
                     {
                         SelectedSegmentIndex = SelectedSegment.Item1;
-                        SelectedPointIndex = SelectedSegment.Item2 ? 0 : 1;
-                        SelectedOffset = (SelectedSegment.Item2 ? Segments[SelectedSegmentIndex].A : Segments[SelectedSegmentIndex].B).MinusF(Center.MinusF(MouseStates.Position));
+                        SelectedOffset = (SelectedSegment.Item2 ? Segments[SelectedSegmentIndex].A : Segments[SelectedSegmentIndex].B).MinusF(ms);
                         mode = SelectedSegment.Item2 ? 3 : 4;
                     }
-                    else if(right_pressed)
+                    else if (right_pressed)
                     {
                         Segments.RemoveAt(SelectedSegment.Item1);
                     }
                 }
+                else
+                    SelectedPointIndex = -1;
             }
 
             if (left_pressed)
@@ -169,16 +176,16 @@ namespace Tooling_Mapper
                 if (mode == 0)
                 {
                     mode = 1;
-                    A = B = MouseStates.Position.MinusF(Center);
+                    A = B = ms;
                 }
             }
             else if (left_down)
             {
                 switch (mode)
                 {
-                    case 1: B = MouseStates.Position.MinusF(Center); break;
-                    case 3: Segments[SelectedSegmentIndex].A = MouseStates.Position.MinusF(Center).PlusF(SelectedOffset); break;
-                    case 4: Segments[SelectedSegmentIndex].B = MouseStates.Position.MinusF(Center).PlusF(SelectedOffset); break;
+                    case 1: B = ms; break;
+                    case 3: Segments[SelectedSegmentIndex].A = ms.PlusF(SelectedOffset); break;
+                    case 4: Segments[SelectedSegmentIndex].B = ms.PlusF(SelectedOffset); break;
                 }
             }
             else
@@ -228,65 +235,52 @@ namespace Tooling_Mapper
 
             void _draw_0(int x)
             {
-                var str = "" + (x - Cam.X);
+                var str = "" + (x + Cam.X);
                 strsz = g.MeasureString(str, fontMini);
                 g.DrawLine(p, wh + x, hh -8, wh + x, hh + 8);
-                g.DrawString(str, fontMini, (x - Cam.X) == 0 ? Brushes.Gray : b, wh + (x == 0 ? 4 : x - strsz.Width / 2F), hh + (x == 0 ? 4 : strsz.Height + 4));
+                g.DrawString(str, fontMini, (x + Cam.X) == 0 ? Brushes.Gray : b, wh + (x == 0 ? 4 : x - strsz.Width / 2F), hh + (x == 0 ? 4 : strsz.Height + 4));
             }
             void _draw_1(int x, int sz) => g.DrawLine(p, wh + x, hh - sz / 2, wh + x, hh + sz / 2);
             void _draw_2(int y)
             {
-                var str = "" + (y - Cam.Y);
+                var str = "" + (y + Cam.Y);
                 strsz = g.MeasureString(str, fontMini);
                 g.DrawLine(p, wh - 8, hh + y, wh + 8, hh + y);
                 if (y != 0)
-                    g.DrawString(str, fontMini, (y - Cam.Y) == 0 ? Brushes.Gray : b, wh + (y == 0 ? 4 : strsz.Width + 4), hh + (y == 0 ? 4 : y - strsz.Height / 2F));
+                    g.DrawString(str, fontMini, (y + Cam.Y) == 0 ? Brushes.Gray : b, wh + (y == 0 ? 4 : strsz.Width + 4), hh + (y == 0 ? 4 : y - strsz.Height / 2F));
             }
             void _draw_3(int y, int sz) => g.DrawLine(p, wh - sz / 2, hh + y, wh + sz / 2, hh + y);
 
             g.DrawLine(p, 0, hh, w, hh);
-            for (int x = (int)Cam.X; x > -wh; x -= 100) _draw_0(x);
-            for (int x = (int)Cam.X; x < wh; x += 100) _draw_0(x);
-            for (int x = (int)Cam.X - 50; x > -wh; x -= 100) _draw_1(x, 10);
-            for (int x = (int)Cam.X + 50; x < wh; x += 100) _draw_1(x, 10);
-            for (int x = (int)Cam.X - 25; x > -wh; x -= 50) _draw_1(x, 5);
-            for (int x = (int)Cam.X - 25; x < wh; x += 50) _draw_1(x, 5);
+            for (int x = -(int)Cam.X; x > -wh; x -= 100) _draw_0(x);
+            for (int x = -(int)Cam.X; x < wh; x += 100) _draw_0(x);
+            for (int x = -(int)Cam.X - 50; x > -wh; x -= 100) _draw_1(x, 10);
+            for (int x = -(int)Cam.X + 50; x < wh; x += 100) _draw_1(x, 10);
+            for (int x = -(int)Cam.X - 25; x > -wh; x -= 50) _draw_1(x, 5);
+            for (int x = -(int)Cam.X - 25; x < wh; x += 50) _draw_1(x, 5);
 
             g.DrawLine(p, wh, 0, wh, h);
-            for (int y = (int)Cam.Y; y > -hh; y -= 100) _draw_2(y);
-            for (int y = (int)Cam.Y; y < hh; y += 100) _draw_2(y);
-            for (int y = (int)Cam.Y - 50; y > -hh; y -= 100) _draw_3(y, 10);
-            for (int y = (int)Cam.Y + 50; y < hh; y += 100) _draw_3(y, 10);
-            for (int y = (int)Cam.Y - 25; y > -hh; y -= 50) _draw_3(y, 5);
-            for (int y = (int)Cam.Y + 25; y < hh; y += 50) _draw_3(y, 5);
+            for (int y = -(int)Cam.Y; y > -hh; y -= 100) _draw_2(y);
+            for (int y = -(int)Cam.Y; y < hh; y += 100) _draw_2(y);
+            for (int y = -(int)Cam.Y - 50; y > -hh; y -= 100) _draw_3(y, 10);
+            for (int y = -(int)Cam.Y + 50; y < hh; y += 100) _draw_3(y, 10);
+            for (int y = -(int)Cam.Y - 25; y > -hh; y -= 50) _draw_3(y, 5);
+            for (int y = -(int)Cam.Y + 25; y < hh; y += 50) _draw_3(y, 5);
         }
         private void DrawSegments()
         {
+            var ms = Center.MinusF(MouseStates.Position).MinusF(Cam);
             var segments = new List<Segment>(Segments);
             foreach (var segment in segments)
             {
                 PointF perp = Maths.Perpendiculaire(segment.A, segment.B);
-                g.DrawLine(Pens.White, Center.PlusF(segment.A), Center.PlusF(segment.B));
-                g.DrawLine(new Pen(Color.FromArgb(100, 0, 0), 8F), Center.PlusF(segment.A.MinusF(perp.x(5F))), Center.PlusF(segment.B.MinusF(perp.x(5F))));
-                if (KB.LeftCtrl)
+                g.DrawLine(Pens.White, Center.PlusF(segment.A).MinusF(Cam), Center.PlusF(segment.B).MinusF(Cam));
+                g.DrawLine(new Pen(Color.FromArgb(100, 0, 0), 8F), Center.PlusF(segment.A.MinusF(perp.x(5F)).MinusF(Cam)), Center.PlusF(segment.B.MinusF(perp.x(5F)).MinusF(Cam)));
+                if (KB.LeftAlt)
                 {
-                    bool col(PointF pt)
-                    {
-                        return Maths.CollisionPointCercle(Center.MinusF(MouseStates.Position), pt.X - point_size / 2F, pt.Y - point_size / 2F, point_size);
-                    }
-                    var segs = Segments.Select(s => (Segments.IndexOf(s), col(s.A), col(s.B))).Where(i => i.Item2 || i.Item3);
-                    (int, bool, bool) SelectedSegment = (-1, false, false);
-                    if (segs.Count() > 0)
-                    {
-                        SelectedSegment = segs.MinBy(i => Maths.Distance(i.Item2 ? Segments[i.Item1].A : Segments[i.Item1].B, Center.MinusF(MouseStates.Position)));
-                        SelectedSegmentIndex = SelectedSegment.Item1;
-                        SelectedPointIndex = SelectedSegment.Item2 ? 0 : 1;
-                        SelectedOffset = (SelectedSegment.Item2 ? Segments[SelectedSegmentIndex].A : Segments[SelectedSegmentIndex].B).MinusF(Center.MinusF(MouseStates.Position));
-                        mode = SelectedSegment.Item2 ? 3 : 4;
-                    }
-                    bool hover = SelectedSegment.Item1 > -1 && Segments[SelectedSegment.Item1] == segment;
-                    g.DrawEllipse(hover && SelectedPointIndex == 0 ? Pens.White : Pens.DarkGray, Center.X + segment.A.X - point_size / 2F, Center.Y + segment.A.Y - point_size / 2F, point_size, point_size);
-                    g.DrawEllipse(hover && SelectedPointIndex == 1 ? Pens.White : Pens.DarkGray, Center.X + segment.B.X - point_size / 2F, Center.Y + segment.B.Y - point_size / 2F, point_size, point_size);
+                    bool hover = SelectedSegment.Item1 != -1 && Segments[SelectedSegment.Item1] == segment;
+                    g.DrawEllipse(hover && SelectedPointIndex == 0 ? Pens.White : Pens.DarkGray, Center.X + segment.A.X - Cam.X - point_size / 2F, Center.Y + segment.A.Y - Cam.Y - point_size / 2F, point_size, point_size);
+                    g.DrawEllipse(hover && SelectedPointIndex == 1 ? Pens.White : Pens.DarkGray, Center.X + segment.B.X - Cam.X - point_size / 2F, Center.Y + segment.B.Y - Cam.Y - point_size / 2F, point_size, point_size);
                 }
             }
         }
@@ -295,14 +289,15 @@ namespace Tooling_Mapper
             if (mode != 1)
                 return;
             PointF perp = Maths.Perpendiculaire(A, B);
-            g.DrawLine(Pens.White, Center.PlusF(A), Center.PlusF(B));
-            g.DrawLine(new Pen(Color.FromArgb(100, 0, 0), 8F), Center.PlusF(A.MinusF(perp.x(5F))), Center.PlusF(B.MinusF(perp.x(5F))));
+            g.DrawLine(Pens.White, Center.PlusF(A).MinusF(Cam), Center.PlusF(B).MinusF(Cam));
+            g.DrawLine(new Pen(Color.FromArgb(100, 0, 0), 8F), Center.PlusF(A.MinusF(perp.x(5F))).MinusF(Cam), Center.PlusF(B.MinusF(perp.x(5F))).MinusF(Cam));
         }
         private void DrawNotifs()
         {
             if(Notifs.Count == 0) return;
             var notif = Notifs.Last();
-            g.DrawString(notif.text, fontNormal, new SolidBrush(Color.FromArgb((byte)((notif_duration - notif.time) / (float)notif_duration * 255F), Color.Yellow)), w / 2, h - 40);
+            var sz = g.MeasureString(notif.text, fontNormal);
+            g.DrawString(notif.text, fontNormal, new SolidBrush(Color.FromArgb((byte)((notif_duration - notif.time) / (float)notif_duration * 255F), Color.Yellow)), w / 2 - sz.Width / 2F, h - 40 - sz.Height / 2F);
             if(notif.time + 1 >= notif_duration)
                 Notifs.RemoveAt(Notifs.Count - 1);
             else
