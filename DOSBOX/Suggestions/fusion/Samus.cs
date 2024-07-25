ce58dc02ce3b683e5257a39a620327aff113dd41
sauveur.dominguez <sauveur.dominguez@zf.com>
2024-07-25 11:09:31 +0200

    fix mvmt

--- DOSBOX/Suggestions/fusion/Samus.cs
+++ DOSBOX/Suggestions/fusion/Samus.cs
@@ -11,6 +11,7 @@ namespace DOSBOX.Suggestions.fusion
         public bool PrevIsMorph, IsMorph = false;
         byte timershot = 0;
         float jump_look_y = 0F;
+        bool is_on_ground = false;
 
         public sbyte ShieldMax = 5;
         public sbyte Shield = 5;
@@ -40,7 +41,11 @@ namespace DOSBOX.Suggestions.fusion
                 PrevIsMorph = IsMorph;
             }
 
+            is_on_ground = Fusion.Instance.CollidesRoom(new vecf(vec.x, vec.y + _h - 2));
+
             float speed = 1F;
+            float input_look_x = (KB.IsKeyDown(KB.Key.Q) ? -1F : 0F) + (KB.IsKeyDown(KB.Key.D) ? 1F : 0F);
+            float input_offset_x = input_look_x == 0 ? 0 : (_w / 2F * (input_look_x < 0F ? -1F : 1F));
             if (jump_look_y == 0F)
             {
                 if (KB.IsKeyDown(KB.Key.Q))
@@ -52,21 +57,25 @@ namespace DOSBOX.Suggestions.fusion
                     move(new vecf(1F, 0F), speed, new vecf(_w / 2F, 0F));
                 }
             }
-            if (KB.IsKeyPressed(KB.Key.Space) && jump_look_y == 0F)
+            if (KB.IsKeyPressed(KB.Key.Space) && is_on_ground && jump_look_y == 0F)
             {
                 jump_look_y = 2F;
             }
             if (jump_look_y > 0F)
             {
-                move(new vecf((KB.IsKeyDown(KB.Key.Q) ? -1F : 0F) + (KB.IsKeyDown(KB.Key.D) ? 1F : 0F), -1F), speed, new vecf(0F, -_h / 2F));
+                move(new vecf(input_look_x, -1F), speed, new vecf(input_offset_x, -_h / 2F));
                 jump_look_y -= 0.2F;
             }
             else
             {
-                if (!move(new vecf((KB.IsKeyDown(KB.Key.Q) ? -1F : 0F) + (KB.IsKeyDown(KB.Key.D) ? 1F : 0F), 1F), speed, new vecf(0F, _h / 2F)))
+                //if (!move(new vecf((KB.IsKeyDown(KB.Key.Q) ? -1F : 0F) + (KB.IsKeyDown(KB.Key.D) ? 1F : 0F), 1F), speed, new vecf(0F, _h / 2F)))
+                if (is_on_ground)
                     jump_look_y = 0F;
                 else
+                {
                     jump_look_y -= 0.2F;
+                    move(new vecf(input_look_x, 1F), speed, new vecf(input_offset_x, (_h-2) / 2F));
+                }
             }
 
             bool T = KB.IsKeyDown(KB.Key.Up);
@@ -110,13 +119,27 @@ namespace DOSBOX.Suggestions.fusion
         /// <returns>false if colliding (then no move)</returns>
         private bool move(vecf look, float speed, vecf offset)
         {
-            offset *= 0.1F;
             if (offset.x == 0F && offset.y == 0F) throw new Exception("jpeux pas faire mon lerp là comme ça");
             bool lerpOnH = offset.y != 0F;
-            bool collides = false;
-            for (float i = 0F; i < speed && !collides; i++)
-                if (!(collides = Fusion.Instance.CollidesRoom(new vecf(vec.x + offset.x + look.x, vec.y + (lerpOnH ? offset.y : 0F) + look.y))))
-                    vec += look;
+            bool collides;
+
+            // y
+            if (!(collides = Fusion.Instance.CollidesRoom(new vecf(vec.x, vec.y + (lerpOnH ? offset.y : 0F) + look.y * speed))))
+                vec.y += look.y;
+
+            // x
+            if (!(collides = Fusion.Instance.CollidesRoom(new vecf(vec.x + offset.x + look.x * speed, vec.y))))
+                vec.x += look.x;
+            else
+            {
+                collides = false;
+                float n = -1F;
+                for (float i = 0.1F; i <= 1.05F && !collides; i += 0.1F)
+                    if (!(collides = Fusion.Instance.CollidesRoom(new vecf(vec.x + offset.x + look.x * i * speed, vec.y))))
+                        n = i;
+                if (collides && n != -1F)
+                    vec.x += look.x * n * speed;
+            }
             //for (float i = 0F; i < speed && !collides; i++)
             //    for (float t = 0F; t <= 1F && !collides; t+=1F/ (lerpOnH?_h:_w))
             //        collides = Instance.CollidesRoom(Maths.Lerp(
